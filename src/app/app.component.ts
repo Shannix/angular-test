67d472d9ee3a4f5bba32d66c8f0a2817ef04ef4d
Amine <shannix@MacBook-Pro-de-Amine.local>
2018-05-27 15:35:27 +0200

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+
   //---------------------------MyCvInformations---------------------------------------------------
 
   name = "DERRADJI";
   firstName = "Amine";
-  date_de_naissance = "29/03/1993";
-  age = getAge(this.date_de_naissance);
+  birthdate = "29/03/1993";
+  age: any = 25;
   phoneNumber = "07 69 17 11 50";
   email = "derradji2993@gmail.com";
   adresse = "28 Rue des rancy";
   completeAdresse = "69003, Lyon";
+  permisB = true;
   citation = "...";
   picture = "profile.jpg";
   job = "Développeur full-stack";
@@ -26,6 +28,7 @@ export class AppComponent {
   hobbies = ["Natation", "Photographie", "VTT", "Escalade"];
   languages = ["Anglais [ Compétence professionnelle ]",
     "Français [ Bilingue ou langue natale ]"];
+
 
   //---------------------------SOCIAL-------------------------------------------------------------
 
@@ -43,14 +46,5 @@ export class AppComponent {
   //----------------------------------------------------------------------------------------------
 
 
-  function getAge(date) {
-
-  var tab = date.split("/");
-  var dob = new Date(tab[2], tab[1] - 1, tab[0]);
-  var today = new Date();
-  var age = Math.floor((today - dob) / (365.25 * 24 * 60 * 60 * 1000));
-
-  return age;
-}
 
 }
