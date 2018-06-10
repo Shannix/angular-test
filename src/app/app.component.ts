0796c9b52b43ed8c92c49d79501b00126b41c7a3
Amine <shannix@MacBook-Pro-de-Amine.local>
2018-06-10 14:35:22 +0200

    Objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,14 +22,28 @@ export class AppComponent {
   citation = "...";
   picture = "profile.jpg";
   job = "Développeur full-stack";
+  whoiam = " Jeune développeur full-stack issu d'une formation en Génie Informatique, je suis à la recherche d'opportunités professionnelles qui me permettraient de m'investir dans des projets innovants à caractère technologique et social. Je suis également passionné par le design graphique.";
+  goal = "à voir ";
   experiences = ["Stage [CAPGEMINI] "];
-  education = ["MASTER 2 [ Génie informatique ] Université Grenoble Alpes"];
-  education2: educationObject[] = [{ id: 1, title: "test", year: "2010" }];
+
+  education: educationObject[] = [
+    { id: 1, title: "Master 2 en Génie informatique", from: "2017", to: "2018", school: "Université Grenoble Alpes" },
+    { id: 2, title: "Master 1 en informatique", from: "2016", to: "2017", school: "" },
+    { id: 3, title: "Licence en informatique", from: "2013", to: "2016", school: "" },
+    { id: 4, title: "baccalauréat S", from: "2012", to: "2013", school: "" }
+  ];
+
   skills = ["Java JEE", "Symfony", "JS", "HTML", "CSS", "Bootstrap"];
+
   hobbies = ["Natation", "Photographie", "VTT", "Escalade"];
+
   languages = ["Anglais [ Compétence professionnelle ]",
     "Français [ Bilingue ou langue natale ]"];
 
+  languages: languageObject[] = [
+    { id: 1, title: "Anglais", level: "80", read: true, write: true, speak: true },
+    { id: 2, title: "Français", level: "100", read: true, write: true, speak: true },
+  ];
 
   //---------------------------SOCIAL-------------------------------------------------------------
 
@@ -52,6 +66,32 @@ export interface educationObject {
 
   id: number;
   title: string;
-  year: string;
+  from: string;
+  to: string;
+  school: string;
+
+
+}
+
+export interface languageObject {
+
+  id: number;
+  title: string;
+  level: string;
+  read: boolean;
+  write: boolean;
+  speak: boolean;
+
+}
+
+export interface experienceObject {
+
+  id: number;
+  title: string;
+  description: string;
+  from: string;
+  to: string;
+  locality: string;
+  technique: string;
 
 }
