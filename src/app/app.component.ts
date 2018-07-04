bbc1ce127f7fd2be1a3bce5f5eedefddb8c0641d
Amine <shannix@MacBook-Pro-de-Amine.local>
2018-07-05 01:01:28 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,26 +18,46 @@ export class AppComponent {
   email = "derradji2993@gmail.com";
   adresse = "28 Rue des rancy";
   completeAdresse = "69003, Lyon";
-  permisB = true;
-  citation = "...";
-  picture = "profile.jpg";
+  permisB = false;
+  citation = "Il y a tellement de domaines qui m'inspire que j'ai envie de devenir un caméléon.";
+  picture = "IMG_1840.JPG";
   job = "Développeur full-stack";
   whoiam = " Jeune développeur full-stack issu d'une formation en Génie Informatique, je suis à la recherche d'opportunités professionnelles qui me permettraient de m'investir dans des projets innovants à caractère technologique et social. Je suis également passionné par le design graphique.";
-  goal = "à voir ";
-  experiences = ["Stage [CAPGEMINI] "];
+  goal = "M'investir dans des projets innovants à caractère technologique et social.";
+  //experiences = ["Stage [CAPGEMINI] "];
+
+  experiences: experienceObject[] = [
+    { id: 1, title: "Stage à [CAPGEMINI]", desc: " Développeur full stack  - intervention sur Cegema-Swisslife et developpement du Responsive mobile/tablette sur SAP Hybris B2B - Spontis ", from: "04-2018", to: "09-2018", locality: "Région de Lyon", technique: "#Liferay #Jhipster #Spring #Java #JSTL #Bootstrap #Salesforce #SQL #JS #JQUERY #CSS #MAVEN #ANT #JIRA #Gitlab" },
+    { id: 2, title: "DOCTORS YOU CAN SPEAK ENGLISH", desc: " Développeur full stack  -  Plateforme web spécialisé dans l’apprentissage de l’anglais médicale destiné aux médecins, dentistes, vétérinaires. ", from: "01-2017", to: "01-2018", locality: "", technique: "#Php Laravel #Angular #JS #Bootstrap #MySql" },
+    { id: 3, title: "Projet E-commerce - déménagement entre particuliers ", desc: " Développeur full stack  -  Plateforme web pour la vente et l’échange entre particuliers lors d’un déménagement.  ", from: "10-2017", to: "01-2018", locality: "", technique: "#Angular #JEE #Ejb3 #Hibernate #MySql" }
+    { id: 4, title: "Projet Jeu Colonne de trois  ", desc: " Conception & développement d’une IA capable de jouer une partie du jeu de colonne de trois et de gagner contre une autre IA. ", from: "05-2017", to: "02-2017", locality: "", technique: "#Prolog #Java #C" }
+
+
+
+
+  ];
+
+
+  id: number;
+  title: string;
+  description: string;
+  from: string;
+  to: string;
+  locality: string;
+  technique: string;
+
 
   education: educationObject[] = [
-    { id: 1, title: "Master 2 en Génie informatique", from: "2017", to: "2018", school: "Université Grenoble Alpes" },
-    { id: 2, title: "Master 1 en informatique", from: "2016", to: "2017", school: "" },
-    { id: 3, title: "Licence en informatique", from: "2013", to: "2016", school: "" },
-    { id: 4, title: "baccalauréat S", from: "2012", to: "2013", school: "" }
+    { id: 1, title: "Master 2 en Génie informatique", from: "2017", to: "2018", school: "Université Grenoble Alpes" }
+
   ];
 
 
 
-  skills = ["Java JEE", "Symfony", "JS", "HTML", "CSS", "Bootstrap"];
+  skills = ["Java J2EE / JSTL", "Liferay", "EJB3", "C/C#/C++", "PHP Symfony", "PHP Laravel", "Ionic", "Angular", "JS", "CSS", "JQUERY," "Bootstrap", "Hibernate", "Mysql", "Firebase", "oracle", "JIRA", "github/lab",];
 
-  hobbies = ["Natation", "Photographie", "VTT", "Escalade"];
+
+  hobbies = ["Natation", "Escalade", "Basket", "Photographie", "Littérature"];
 
 
   languages: languageObject[] = [
@@ -75,7 +95,7 @@ export class AppComponent {
   FB = "#";
   LINKDIN = "https://www.linkedin.com/in/amine-derradji-a4b8b7133/";
   INSTA = "#";
-  GIT = "https:/github.com/Shannix";
+  GIT = "https://github.com/Shannix";
   TWITTER = "#";
   GOOGLEPLUS = "#";
 
